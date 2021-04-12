2310bf9ae63c6335cabaad991fcb68572c93dbff
ntgrd <72499903+ntgrd@users.noreply.github.com>
2021-04-12 10:45:59 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+const products = [
+    { id: 1, title: 'Notebook', price: 2000, img: 'img/card-1.png' },
+    { id: 2, title: 'Mouse', price: 20, img: 'img/card-1.png' },
+    { id: 3, title: 'Keyboard', price: 200, img: 'img/card-1.png' },
+    { id: 4, title: 'Gamepad', price: 50, img: 'img/card-1.png' },
+];
+//Функция для формирования верстки каждого товара
+const renderProduct = (item) => {
+    return `<div class="product-item">
+                <h3>${item.title}</h3>
+                <img src=${item.img} alt="photo">
+                <p>${item.price}</p>
+                <button class="buy-btn">Купить</button>
+            </div>`
+};
+
+const renderPage = list => {
+    const productsList = list.map(item => renderProduct(item));
+    console.log(productsList);
+    document.querySelector('.products').innerHTML = productsList.join('');
+};
+
+renderPage(products);
